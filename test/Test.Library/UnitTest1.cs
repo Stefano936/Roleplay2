331428412c5ea55350700617517d0b66f47b59b0
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-09-20 10:22:52 -0300

    .

--- test/Test.Library/UnitTest1.cs
+++ test/Test.Library/UnitTest1.cs
@@ -4,15 +4,65 @@ namespace Test.Library
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
+        namespace Program
+{
+    public class RoleplayGameTests
+    {
+        /*
+            JUSTIFICACIÓN:
+            Este caso de prueba lo que revisa es si, al agregar un aspecto tan importante
+            como el nombre de un personaje, es vacio o nulo.
+            Si el llamado de la función devuelve TRUE, significa que el nombre pasó las validaciones correspondientes.
+            Si devuelve FALSE, es sinonimo que el nombre no es valido para el personaje. 
+        */
+        [Test]
+        public void TestBlankWizardName() {
+            Wizard wiz = new Wizard();
+            string newWizardName = "";
+
+            bool result = wiz.SetName(newWizardName);
+            Assert.False(result);
         }
 
+        /*
+            JUSTIFICACIÓN:
+            Este test se encarga de realizar una prueba, de modo que no pueden haber 2 o 
+            más hechizos con el mismo nombre, ya que si el nombre se repite 2 veces,
+            el result2(último hechizo agregado) se considerará falso
+        */
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+        public void TestDuplicatedSpell(){
+            SpellBook book = new SpellBook();
+            Spell sp = new Spell("Heal");
+
+            bool result = book.AddSpell(sp);
+            bool result2 = book.AddSpell(sp);
+            Assert.False(result2);
+        }
+
+        /*
+            JUSTIFICACIÓN:
+            Este test verifica que el costo de los items no sea un número negativo,
+            si es, devuelve false, de lo contrario se guarda el valor asignado del costo.
+        */
+        [Test]
+        public void TestNegativeCost(){
+            Item hacha = new Item("hacha", "Arma", -50, 15, 0);
+            Assert.False(hacha.SetCost(-50));
+        }
+        /*
+            JUSTIFICACIÓN:   
+            Este test verifica que un mago no tenga más de un libro de hechizos, ya que este
+            es unico y personal de cada mago.
+        */ 
+        [Test]
+        public void TestDuplicatedSpellBook(){
+            SpellBook sps=new SpellBook();
+            Wizard wiz= new Wizard();
+
+            bool result= wiz.SetSpellBook(sps);
+            bool result2=wiz.SetSpellBook(sps);
+            Assert.False(result2);
         }
     }
 }
