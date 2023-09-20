79f63999d40cbee491f8b1e94a5cd131e42f5c0e
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-09-20 09:18:34 -0300

    Ejercicio

--- src/Library/Characters/Dwarf.cs
+++ src/Library/Characters/Dwarf.cs
@@ -1,22 +1,17 @@
 namespace RoleplayGame
 {
-    public class Dwarf : ICharacter
+    public class Dwarf: ICharacter
     {
         private int health = 100;
-
         public Dwarf(string name)
         {
             this.Name = name;
         }
-
         public string Name { get; set; }
-
         public Axe Axe { get; set; }
-
         public Shield Shield { get; set; }
-
         public Helmet Helmet { get; set; }
-
+        public Bow Bow { get; set; }
         public int AttackValue
         {
             get
@@ -24,7 +19,6 @@ namespace RoleplayGame
                 return Axe.AttackValue;
             }
         }
-
         public int DefenseValue
         {
             get
@@ -32,7 +26,6 @@ namespace RoleplayGame
                 return Shield.DefenseValue + Helmet.DefenseValue;
             }
         }
-
         public int Health
         {
             get
@@ -44,7 +37,6 @@ namespace RoleplayGame
                 this.health = value < 0 ? 0 : value;
             }
         }
-
         public void ReceiveAttack(int power)
         {
             if (this.DefenseValue < power)
@@ -52,7 +44,6 @@ namespace RoleplayGame
                 this.Health -= power - this.DefenseValue;
             }
         }
-
         public void Cure()
         {
             this.Health = 100;
